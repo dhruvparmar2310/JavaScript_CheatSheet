725ea41537244217cf78dd5d45c1bf49ca323454
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-02-28 12:11:35 +0530

    Create create.js

---
+++ Object Methods/create.js
@@ -0,0 +1,30 @@
+/*
+Object.create(proto)
+Object.create(proto, propertiesObject)
+
+It will return new Object with specified properties.
+*/
+
+
+const person = {
+    printIntroduction: function () {
+        console.log(`My name is ${this.name} and I am ${this.age} years old.`)
+    }
+}
+
+const me = Object.create(person, {
+    foo: {
+        writable: false,
+        configurable: true,
+        value: "hello",
+  },
+})
+
+me.name = 'Dhruv Parmar';
+me.age = 22
+
+me.foo = 'Object.create() demo' // it will not overwrite the value because it is read-only.
+
+
+me.printIntroduction()
+console.log(me, person)
