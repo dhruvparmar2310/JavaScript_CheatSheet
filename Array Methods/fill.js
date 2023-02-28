f83a59b9a9b5170d0fe9443f810c38c899134729
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-02-28 12:07:51 +0530

    Create fill.js

---
+++ Array Methods/fill.js
@@ -0,0 +1,6 @@
+// it will return modified array, filled with values. It will not change the length of array.
+const fruits = ['Mango', 'Apple', 'Strawberry', 'Orange', 'Green Apple', 'Kiwi', 'Banana']
+
+//console.log(fruits.fill({name: "Dhruv", age: 22}))
+//console.log(fruits.fill('Blueberry', 2))
+console.log(fruits.fill('Blueberry', 0, 2))
