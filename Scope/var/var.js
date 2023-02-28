e82c9ccdb7b5ab897f0e27bf2dcf4b942b09deb1
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-02-28 14:48:15 +0530

    Update var.js

--- Scope/var/var.js
+++ Scope/var/var.js
@@ -7,4 +7,9 @@ function getCarCompany () {
   var carCompany = 'Ford'
 }
 
-console.log(carCompany) // As we know 'var' is global scope but it cannot be accessed outside the function scope, if it is assigned inside the function. 
+console.log(carCompany) // throws an error of: carCompany is not defined 
+// As we know 'var' is global scope but it cannot be accessed outside the function scope, if it is assigned inside the function. 
+
+//without declaring var, it will take globally by default
+carColor = 'Red'
+console.log(carColor) // Red
