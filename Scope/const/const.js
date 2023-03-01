14ea1f3f07aa23927ec4728e0030ea3b563a8266
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-03-01 12:14:33 +0530

    Update const.js

--- Scope/const/const.js
+++ Scope/const/const.js
@@ -1,3 +1,9 @@
 //example-1: it cannot be redeclared or reassigned
 const x = 10;
 const x = 50;
+
+
+//example-2: 
+const x = 10;
+x = 'Dhruv'
+console.log(x); // TypeError: Assignment to constant variable.
