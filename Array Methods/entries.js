da06898b64ca3cb2a1533fe4c4a691a9813e06f5
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-03-07 09:56:36 +0530

    updating the generator and iterator section

--- Array Methods/entries.js
+++ Array Methods/entries.js
@@ -6,6 +6,7 @@ const iterator = fruits.entries()
 
 
 //console.log(iterator) // returns empty object
+console.log(iterator.next()) // Object { value: Array [0, 'Mango'], done: false }
 //console.log(iterator.next().value) // will return an array of key/value pair of first index.
 //console.log(iterator.next().value)
 //console.log(iterator.next().value)
@@ -18,3 +19,7 @@ const iterator = fruits.entries()
 for (let [index, elements] of iterator) {
     console.log(index, elements)
 }
+
+/*
+Tips: Also see the documentation for generator and iterator.
+*/
