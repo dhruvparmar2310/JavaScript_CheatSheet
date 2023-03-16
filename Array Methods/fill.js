096f4bff045e0ac6490f6930b1c2474a653462e1
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2023-03-16 12:39:13 +0530

    created behind the hood method

--- Array Methods/fill.js
+++ Array Methods/fill.js
@@ -3,4 +3,21 @@ const fruits = ['Mango', 'Apple', 'Strawberry', 'Orange', 'Green Apple', 'Kiwi',
 
 //console.log(fruits.fill({name: "Dhruv", age: 22}))
 //console.log(fruits.fill('Blueberry', 2))
-console.log(fruits.fill('Blueberry', 0, 2))
+//console.log(fruits.fill('Blueberry', 0, 2))
+
+// behind the hood
+function fillMethod (arr, value, start, end) {
+   console.log(arr, value, start)
+   for (let ele of arr) {
+      console.log(ele)
+   }
+   
+   let newArr=[]
+    for (let x = start; x < arr.length; x++) {
+       arr[x] = value
+       newArr.push(arr[x])
+   }
+   return newArr
+}
+
+console.log(fillMethod(['a', 'b', 'c', 'd', 'e'], 'e', 3))
